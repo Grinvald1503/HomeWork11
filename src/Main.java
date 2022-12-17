99e9be889b71e166dd05093a4fea51340cc20d14
Aleksandr <aleksanches123@mail.ru>
2022-12-17 12:29:17 +0300

    Внёс изменения

--- src/Main.java
+++ src/Main.java
@@ -11,57 +11,46 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         int year = 2021;
-        System.out.println(year + findOutWhatYear(year));
+        findOutWhatYear(year);
     }
 
-    public static String findOutWhatYear(int year) {
-        String whatYear;
-        if (year % 400 == 0) {
-            whatYear = " год — високосный год";
-        } else if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                whatYear = " год — невисокосный год";
-            } else {
-                whatYear = " год — високосный год";
-            }
+    public static void findOutWhatYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год — високосный год");
         } else {
-            whatYear = " год — невисокосный год";
+            System.out.println(year + " год — невисокосный год");
         }
-        return whatYear;
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         byte clientOS = 0;
         int clientDeviceYear = LocalDate.now().getYear();
-        System.out.println(findOutTheVersion(clientOS, clientDeviceYear));
+        findOutTheVersion(clientOS, clientDeviceYear);
     }
 
-    public static String findOutTheVersion(int OS, int version) {
-        String resultOS = "";
-        String resultVersion = "";
-        if (OS == 0) {
-            resultOS = "Установите версию приложения для IOS по ссылке \n";
+    public static void findOutTheVersion(int oS, int version) {
+        if (oS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
             if (version < 2015) {
-                resultVersion = ("Советуем установить облегченную версию приложения для IOS по ссылке\n");
+                System.out.println("Советуем установить облегченную версию приложения для IOS по ссылке");
             }
-        } else if (OS == 1) {
-            resultOS = ("Установите версию приложения для Android по ссылке \n");
+        } else if (oS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
             if (version < 2015) {
-                resultVersion = ("Советуем установить облегченную версию приложения для Android по ссылке\n");
+                System.out.println("Советуем установить облегченную версию приложения для Android по ссылке");
             }
         }
-        return resultOS + resultVersion;
     }
 
     public static void task3() {
         System.out.println("Задача 3");
-        int deliveryDistance = 95;
+        int deliveryDistance = 30;
         String result = "Потребуется дней: ";
         System.out.println(result + needDays(deliveryDistance));
     }
 
-    public static int needDays (int days) {
+    public static int needDays(int days) {
         int day;
         int dayAdded = (days - 20) / 40 + 1;
         int daysForDelivery = dayAdded + 1;
